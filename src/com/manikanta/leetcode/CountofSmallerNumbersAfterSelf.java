4c2e40bffff0a1f71391b2acb757c592cc366ce8
Manikanta528 <chmanikanta528@gmail.com>
2022-07-23 21:33:25 +0530

    CountofSmallerNumbersAfterSelf

---
+++ src/com/manikanta/leetcode/CountofSmallerNumbersAfterSelf.java
@@ -0,0 +1,26 @@
+package com.manikanta.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountofSmallerNumbersAfterSelf {
+    public static void main(String[] args) {
+        int nums[] = {-1,-1};
+        System.out.println(countSmaller(nums));
+    }
+    public static List<Integer> countSmaller(int[] nums) {
+
+        // brute force
+        List<Integer> list = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n-1; i++) {
+            int count = 0;
+            for (int j = i+1; j < n ; j++) {
+                if(nums[i] > nums[j]) count++;
+            }
+            list.add(count);
+        }
+        list.add(0);
+        return list;
+    }
+}
