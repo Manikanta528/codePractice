6e1d906d605b6ebbadfc6f1fd5cdd3f69d1319d2
Manikanta528 <chmanikanta528@gmail.com>
2022-07-25 21:10:21 +0530

    iteration

--- src/com/manikanta/leetcode/ReverseString.java
+++ src/com/manikanta/leetcode/ReverseString.java
@@ -11,9 +11,19 @@ class ReverseString {
     public static void reverseString(char[] s) {
         int start = 0;
         int end = s.length-1;
-        helper(s,start,end);
+        //recursion
+        //helper(s,start,end);
+        while(start < end){
+            //iteration
+            char ch = s[start];
+            s[start] = s[end];
+            s[end] = ch;
+            end--;
+            start++;
+        }
     }
     public static void helper(char[] s, int start,int end){
+        //recursion
         if(start == s.length/2) return;
         char ch = s[start];
         s[start] = s[end];
