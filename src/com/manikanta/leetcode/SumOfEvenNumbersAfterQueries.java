bba1d7dfd0a2f04e0b641cb69c4faee0f4637c5f
Manikanta528 <chmanikanta528@gmail.com>
2022-09-21 18:46:29 +0530

    Q.985 Sum of Even Numbers After Queries

---
+++ src/com/manikanta/leetcode/SumOfEvenNumbersAfterQueries.java
@@ -0,0 +1,33 @@
+package com.manikanta.leetcode;
+
+import java.util.Arrays;
+
+public class SumOfEvenNumbersAfterQueries {
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,4};
+        int queries[][] = {{1,0},{-3,1},{-4,0},{2,3}};
+        int arr[] = sumEvenAfterQueries(nums, queries);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static int[] sumEvenAfterQueries(int[] nums, int[][] queries){
+        int arr[] = new int[queries.length];
+        int sumEven = 0,p = 0;
+        for(int i : nums){
+            if(i%2 == 0){
+                sumEven += i;
+            }
+        }
+        for(int i = 0 ; i < queries.length ; i++){
+            if(nums[queries[i][1]]%2 == 0){
+                sumEven -= nums[queries[i][1]];
+            }
+            nums[queries[i][1]] += queries[i][0];
+            if(nums[queries[i][1]]%2 == 0){
+                sumEven += nums[queries[i][1]] ;
+            }
+            arr[i] = sumEven;
+        }
+        return arr;
+
+    }
+}
