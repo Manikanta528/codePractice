7d30d94e827058ad90738095c448e0e81315239c
Manikanta528 <chmanikanta528@gmail.com>
2022-09-22 05:46:28 +0530

    Q.557. Reverse Words in a String III

--- src/com/manikanta/leetcode/ReverseWordsInAStringIII.java
+++ src/com/manikanta/leetcode/ReverseWordsInAStringIII.java
@@ -1,5 +1,7 @@
 package com.manikanta.leetcode;
 
+//https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
 
 public class ReverseWordsInAStringIII {
     public static void main(String[] args) {
@@ -7,13 +9,28 @@ public class ReverseWordsInAStringIII {
         System.out.println(reverseWords(str));
     }
     public static String reverseWords(String s){
+//        String str1 = "";
+//        String arr[] = s.split(" ");
+//        int len = arr.length;
+//        for (int i = 0; i < len; i++) {
+//            for (int j = arr[i].length()-1; j >=0 ; j--) {
+//                str1 += arr[i].charAt(j);
+//            }
+//            if(i != len-1) str1 +=" ";
+//        }
+//        return str1;
+
         String str1 = "";
         String arr[] = s.split(" ");
         int len = arr.length;
+        StringBuilder temp = new StringBuilder();
         for (int i = 0; i < len; i++) {
-            for (int j = arr[i].length()-1; j >=0 ; j--) {
-                str1 += arr[i].charAt(j);
-            }
+            // for (int j = arr[i].length()-1; j >=0 ; j--) {
+            //     str1 += arr[i].charAt(j);
+            // }
+            temp = new StringBuilder(arr[i]);
+            temp.reverse();
+            str1 += temp.toString();
             if(i != len-1) str1 +=" ";
         }
         return str1;
