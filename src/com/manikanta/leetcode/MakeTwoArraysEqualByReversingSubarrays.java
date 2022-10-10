ccd035d9473975a2b186680556cc28d747bb672a
Manikanta528 <chmanikanta528@gmail.com>
2022-10-10 21:20:05 +0530

    Q.1460 Make Two Arrays Equal by Reversing Subarrays

---
+++ src/com/manikanta/leetcode/MakeTwoArraysEqualByReversingSubarrays.java
@@ -0,0 +1,29 @@
+package com.manikanta.leetcode;
+
+import java.util.HashMap;
+
+public class MakeTwoArraysEqualByReversingSubarrays {
+    public static void main(String[] args) {
+        int target[] = {1,2,3,4};
+        int arr[] = {2,4,1,3};
+        System.out.println(canBeEqual(target,arr));
+    }
+    public static boolean canBeEqual(int[] target, int[] arr) {
+        HashMap<Integer,Integer> mapTarget = new HashMap<>();
+        HashMap<Integer,Integer> mapArr = new HashMap<>();
+        for(int i : target){
+            mapTarget.putIfAbsent(i,0);
+            mapTarget.put(i,mapTarget.get(i)+1);
+        }
+        for(int i : arr){
+            mapArr.putIfAbsent(i,0);
+            mapArr.put(i,mapArr.get(i)+1);
+        }
+        if(mapArr.size() != mapTarget.size()) return false;
+        for(int i : target){
+            if(mapArr.get(i) != mapTarget.get(i) ) return false;
+        }
+        return true;
+    }
+}
+
